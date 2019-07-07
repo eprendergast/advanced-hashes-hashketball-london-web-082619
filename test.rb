4d05e89cdd33d91d69199f7584d11942a49e61c0
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-07 13:47:12 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ game_hash = {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
-      "Alan Anderson" => {
+      {"Alan Anderson" => {
         :number => 0,
         :shoe => 16,
         :points => 22,
@@ -14,8 +14,8 @@ game_hash = {
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 1
-      },
-      "Reggie Evans" => {
+      }},
+      {"Reggie Evans" => {
         :number => 30,
         :shoe => 14,
         :points => 12,
@@ -24,8 +24,8 @@ game_hash = {
         :steals => 12,
         :blocks => 12,
         :slam_dunks => 7
-      },
-      "Brook Lopez" => {
+      }},
+      {"Brook Lopez" => {
         :number => 11,
         :shoe => 17,
         :points => 17,
@@ -34,8 +34,8 @@ game_hash = {
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 15
-      },
-      "Mason Plumlee" => {
+      }},
+      {"Mason Plumlee" => {
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -44,8 +44,8 @@ game_hash = {
         :steals => 3,
         :blocks => 8,
         :slam_dunks => 5
-      },
-      "Jason Terry" => {
+      }},
+      {"Jason Terry" => {
         :number => 31,
         :shoe => 15,
         :points => 19,
@@ -54,7 +54,7 @@ game_hash = {
         :steals => 4,
         :blocks => 11,
         :slam_dunks => 1
-      }
+      }}
     ]
   },
   :away => {
