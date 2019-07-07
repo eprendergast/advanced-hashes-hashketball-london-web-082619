adb2999408e2f866f075b38a7e1ad461dc6d618c
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-07 14:20:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -198,7 +198,7 @@ end
 
 #returns a hash of that player's stats
 def player_stats(player)
-  player_stats
+  player_stats = {}
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if attribute == :players
